f4bdbafa0eeff2d0c4cd707a127c9b6c290fdaa7
Fredrik Sethne-Andersen <fredrik.sethne-andersen@sempro.no>
2018-03-02 15:24:46 +0100

    updated

--- front.sh
+++ front.sh
@@ -1,46 +1,42 @@
-semproFrontendDownloader() {
- echo ""
- echo "=> Cloning repository from Github"
- git clone git@github.com:Sempro/front-end_style_guide.git
+echo ""
+echo "=> Cloning repository from Github"
+git clone git@github.com:Sempro/front-end_style_guide.git
 
- echo ""
- echo "=> Extracting files"
- rsync -av front-end_style_guide/resources/assets resources
- mv front-end_style_guide/.stylelintrc ./
- mv front-end_style_guide/.nvmrc ./
- if [ ! -f package.json ]; then
-  mv front-end_style_guide/package.json ./
- fi
- if [ ! -f webpack.mix.js ]; then
-  mv front-end_style_guide/webpack.mix.js ./
- fi
+echo ""
+echo "=> Extracting files"
+rsync -av front-end_style_guide/resources/assets resources
+mv front-end_style_guide/.stylelintrc ./
+mv front-end_style_guide/.nvmrc ./
+if [ ! -f package.json ]; then
+mv front-end_style_guide/package.json ./
+fi
+if [ ! -f webpack.mix.js ]; then
+mv front-end_style_guide/webpack.mix.js ./
+fi
 
- echo ""
- echo "=> Removing git repository"
- rm -rf front-end_style_guide
+echo ""
+echo "=> Removing git repository"
+rm -rf front-end_style_guide
 
- echo ""
- echo "=> Removing unused files"
- rm -f resources/assets/sass/README.md
- rm -f bootstrap-grid-debug.css
+echo ""
+echo "=> Removing unused files"
+rm -f resources/assets/sass/README.md
+rm -f bootstrap-grid-debug.css
 
- echo ""
- echo "=> Setting node version"
- nvm use
+echo ""
+echo "=> Setting node version"
+nvm use
 
- echo ""
- echo "=> Installing packages"
- npm install jquery --save
- npm install popper.js --save
- npm install bootstrap@4.0.0 --save
- npm install laravel-mix --save-dev
- npm install cross-env --save-dev
- npm install stylelint --save-dev
- npm install stylelint-config-standard --save-dev
- npm install stylelint-order --save-dev
+echo ""
+echo "=> Installing packages"
+npm install jquery --save
+npm install popper.js --save
+npm install bootstrap@4.0.0 --save
+npm install laravel-mix --save-dev
+npm install cross-env --save-dev
+npm install stylelint --save-dev
+npm install stylelint-config-standard --save-dev
+npm install stylelint-order --save-dev
 
- echo ""
- echo "=> All done"
-}
-
-semproFrontendDownloader
+echo ""
+echo "=> All done"
