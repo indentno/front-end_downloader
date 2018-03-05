a3b59facb65b1d047cb8157570f629af9eed3962
Fredrik Sethne-Andersen <fredrik.sethne-andersen@sempro.no>
2018-03-05 13:03:32 +0100

    Added eslint front @ecrmnn to v2 test-file

---
+++ v2.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo ""
+echo "=> Cloning repository from Github"
+git clone git@github.com:Sempro/front-end_style_guide.git
+
+echo ""
+echo "=> Extracting files"
+rsync -av front-end_style_guide/resources/assets resources
+mv front-end_style_guide/.stylelintrc ./
+mv front-end_style_guide/.nvmrc ./
+if [ ! -f package.json ]; then
+mv front-end_style_guide/package.json ./
+fi
+if [ ! -f webpack.mix.js ]; then
+mv front-end_style_guide/webpack.mix.js ./
+fi
+
+echo ""
+echo "=> Removing git repository"
+rm -rf front-end_style_guide
+
+echo ""
+echo "=> Removing unused files"
+rm -f resources/assets/sass/README.md
+rm -f bootstrap-grid-debug.css
+
+echo ""
+echo "=> Setting node version"
+. ~/.nvm/nvm.sh
+nvm use
+
+echo ""
+echo "=> Installing packages"
+npm install jquery popper.js bootstrap@4.0.0 --save
+npm install laravel-mix cross-env stylelint stylelint-config-standard stylelint-config-standard stylelint-order --save-dev
+
+echo ""
+echo "=> Installing eslint"
+bash <(curl https://raw.githubusercontent.com/ecrmnn/eslintrc.js/master/init.sh)
+
+echo ""
+echo "=> All done"
